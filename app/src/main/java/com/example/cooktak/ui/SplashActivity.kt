507d038c0295bddb4e3c999b7af3b32f3b85241c
lenox24 <yk03171@naver.com>
2019-06-13 23:51:51 +0900

    Import from previous project

---
+++ app/src/main/java/com/example/cooktak/ui/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cooktak.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.example.cooktak.R
+
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashTheme)
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}

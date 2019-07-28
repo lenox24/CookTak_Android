5cbbe2622d0e4cc082f5c6c8b9c2ed8ae1967896
lenox24 <yk03171@naver.com>
2019-07-28 23:54:09 +0900

    rewrite rvadapter

--- app/src/main/java/com/example/cooktak/Adapter/RecommendRVAdapter.kt
+++ app/src/main/java/com/example/cooktak/Adapter/RecommendRVAdapter.kt
@@ -1,16 +1,17 @@
 package com.example.cooktak.Adapter
 
+import android.app.AlertDialog
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.Toast
 import com.example.cooktak.R
 import com.example.cooktak.model.RecommendModel
 import kotlinx.android.synthetic.main.community_rv_item.view.*
 import kotlinx.android.synthetic.main.community_rv_item2.view.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class RecommendRVAdapter(
     val context: Context,
@@ -34,18 +35,37 @@ class RecommendRVAdapter(
     override fun getItemCount(): Int = viewList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var starFlag: Boolean = false
-
         holder.bindView(viewList[position])
         if (!flag) {
-            if (position == expandedPosition) {
-                holder.expandItem.visibility = View.INVISIBLE
-            } else {
-                holder.expandItem.visibility = View.GONE
+            var starFlag = false
+            holder.itemView.img_star2.onClick {
+                if (starFlag) {
+                    val builder = AlertDialog.Builder(context)
+                    builder.setMessage("즐겨찾기를 취소하시곘습니까?")
+                    builder.setPositiveButton("네") { _, _ ->
+                        starFlag = !starFlag
+                    }.setNegativeButton("아니오") { _, _ -> }.show()
+
+                } else {
+                    starFlag = !starFlag
+                }
+                if (starFlag) {
+                    holder.itemView.img_star2.setImageResource(R.drawable.star1_error)
+                } else {
+                    holder.itemView.img_star2.setImageResource(R.drawable.star2_error)
+                }
             }
         }
 
         holder.itemView.setOnClickListener {
+            if (!flag) {
+                if (position == expandedPosition) {
+                    holder.expandItem!!.visibility = View.INVISIBLE
+                } else {
+                    holder.expandItem!!.visibility = View.GONE
+                }
+            }
+
             if (flag) {
                 Toast.makeText(context, "Title : ${holder.itemView.tex_title1.text}", Toast.LENGTH_SHORT).show()
             } else {
@@ -59,21 +79,10 @@ class RecommendRVAdapter(
                 notifyItemChanged(expandedPosition)
             }
         }
-
-        holder.star.setOnClickListener {
-            starFlag = !starFlag
-
-            if (starFlag == false) {
-                holder.star.setImageResource(R.drawable.star2_error)
-            } else {
-                holder.star.setImageResource(R.drawable.star1_error)
-            }
-        }
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val expandItem = itemView.findViewById<View>(R.id.expand_constraint)
-        val star = itemView.findViewById<ImageView>(R.id.img_star2)
+        val expandItem: View? = itemView.findViewById(R.id.expand_constraint_layout)
 
         fun bindView(recommendModel: RecommendModel) {
             if (flag) {
